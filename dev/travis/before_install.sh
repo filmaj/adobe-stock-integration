86cffcb1e6575469ca2a4569dd79207737b77d3f
filmaj <maj.fil@gmail.com>
2019-11-11 14:49:30 -0500

    first pass at travis functional tests

--- dev/travis/before_install.sh
+++ dev/travis/before_install.sh
@@ -24,3 +24,34 @@ test -n "$GITHUB_TOKEN" && composer config github-oauth.github.com "$GITHUB_TOKE
 
 composer config --global http-basic.repo.magento.com "$MAGENTO_USERNAME" "$MAGENTO_PASSWORD"
 git clone https://github.com/magento/magento2
+
+if [[ ${TEST_SUITE} = "functional" ]]; then
+    # Install apache
+    sudo apt-get update
+    sudo apt-get install apache2 libapache2-mod-fastcgi
+    if [ ${TRAVIS_PHP_VERSION:0:1} == "7" ]; then
+        sudo cp ${TRAVIS_BUILD_DIR}/magento2/dev/travis/config/www.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/
+    fi
+
+    # Enable php-fpm
+    sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+    sudo a2enmod rewrite actions fastcgi alias
+    echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+
+    # Configure apache virtual hosts
+    sudo cp -f ${TRAVIS_BUILD_DIR}/magento2/dev/travis/config/apache_virtual_host /etc/apache2/sites-available/000-default.conf
+    sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)/magento2?g" --in-place /etc/apache2/sites-available/000-default.conf
+    sudo sed -e "s?%MAGENTO_HOST_NAME%?${MAGENTO_HOST_NAME}?g" --in-place /etc/apache2/sites-available/000-default.conf
+
+    sudo usermod -a -G www-data travis
+    sudo usermod -a -G travis www-data
+
+    phpenv config-rm xdebug.ini
+    sudo service apache2 restart
+
+    /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :1 -screen 0 1280x1024x24
+fi
+
+echo 'Creating magento2 database'
+mysql -uroot -e 'CREATE DATABASE magento2;'
