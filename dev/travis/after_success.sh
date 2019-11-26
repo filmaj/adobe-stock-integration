5b45ef7199776bcabbaa187f89bdd43a7b0400de
filmaj <maj.fil@gmail.com>
2019-11-26 11:26:14 -0500

    move after success back to travis file as travis_retry is only available in there

--- dev/travis/after_success.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Copyright © Magento, Inc. All rights reserved.
-# See COPYING.txt for license details.
-
-if [ $TEST_SUITE == 'unit' ]; then
-    travis_retry coveralls;
-fi
