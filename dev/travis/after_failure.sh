e97ce7a4fefac5dbe8a3495e200ddc833a71940a
filmaj <maj.fil@gmail.com>
2019-11-26 13:03:05 -0500

    wetf

--- dev/travis/after_failure.sh
+++ dev/travis/after_failure.sh
@@ -9,13 +9,11 @@ if [ $TEST_SUITE == 'functional' ]; then
     cat "${TRAVIS_BUILD_DIR}/magento2/var/log/debug.log";
     cat "${TRAVIS_BUILD_DIR}/magento2/var/log/system.log";
     pushd "${TRAVIS_BUILD_DIR}/magento2/dev/tests/acceptance/tests/_output";
-    set +x
     for screenshot in *.png;
     do
         echo "Converting and uploading ${screenshot}..."
         IMAGEDATA=$(base64 $screenshot);
         curl --location --request POST --form "image=${IMAGEDATA}" "https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}"
     done
-    set -x
     popd
 fi
