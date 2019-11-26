168e8f680909a60b348da94c6438e54793a2b1c8
filmaj <maj.fil@gmail.com>
2019-11-26 12:29:44 -0500

    a bit quieter during screenshot upload

--- dev/travis/after_failure.sh
+++ dev/travis/after_failure.sh
@@ -9,10 +9,13 @@ if [ $TEST_SUITE == 'functional' ]; then
     cat "${TRAVIS_BUILD_DIR}/magento2/var/log/debug.log";
     cat "${TRAVIS_BUILD_DIR}/magento2/var/log/system.log";
     pushd "${TRAVIS_BUILD_DIR}/magento2/dev/tests/acceptance/tests/_output";
+    set +x
     for screenshot in *.png;
     do
+        echo "Converting and uploading ${screenshot}..."
         IMAGEDATA=$(base64 $screenshot);
         curl --location --request POST "https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}" --form "image=${IMAGEDATA}"
     done
+    set -x
     popd
 fi
